8f9a908b10782c9313d161373ac4294394d640c0
Santiago Botta <sgobotta@gmail.com>
2021-07-24 19:11:56 -0300

    :test_tube: Adds a dummy test

---
+++ apps/ex_commerce/test/ex_commerce/ex_commerce_test.exs
@@ -0,0 +1,13 @@
+defmodule ExCommerce.ExCommerceTest do
+  @moduledoc false
+
+  use ExUnit.Case
+
+  describe "ex commerce" do
+
+    test "hello_world/0" do
+      assert true
+    end
+
+  end
+end
