51120d4f5d4d9bc0579aadb5fdd0464e3e81868b
Santiago <santiago@camba.coop>
2021-08-23 00:59:20 -0300

    :computer: Adds a dashboard and logout button views

--- apps/ex_commerce_web/lib/ex_commerce_web/views/buttons/buttons_view.ex
+++ apps/ex_commerce_web/lib/ex_commerce_web/views/buttons/buttons_view.ex
@@ -1,4 +1,4 @@
-defmodule ExCommerceWeb.Buttons do
+defmodule ExCommerceWeb.ButtonsView do
   @moduledoc false
 
   use ExCommerceWeb, :view
@@ -7,8 +7,17 @@ defmodule ExCommerceWeb.Buttons do
     render("login_button.html", [])
   end
 
+  def render("logout_button", assigns) do
+    opts = Map.put_new(assigns, :classes, "")
+    render("logout_button.html", opts)
+  end
+
   def render("register_button", _assigns) do
     render("register_button.html", [])
+  end
+
+  def render("admin_dashboard_button", _assigns) do
+    render("admin_dashboard_button.html", [])
   end
 
   def render("icon_button", _assigns) do
