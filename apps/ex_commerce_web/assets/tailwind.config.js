a4c76810ab5b0af666773224fdc88c0505db21b0
Santiago <santiago@camba.coop>
2021-08-27 00:37:11 -0300

    Navbar improvements

--- apps/ex_commerce_web/assets/tailwind.config.js
+++ apps/ex_commerce_web/assets/tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      borderWidth: {
+        '1': '1px'
+      },
       boxShadow: {
         'button': '5px 5px 7px 0px rgba(0, 0, 0, 0.3)'
       },
@@ -59,8 +62,7 @@ module.exports = {
     },
     fontFamily: {
       'sans': ['Helvetica', 'Arial', 'sans-serif']
-    },
-    zIndex: {}
+    }
   },
   variants: {
     extend: {
