11cc640836ef6efa03f7fa560854c6656dc23156
Santiago Botta <sgobotta@gmail.com>
2021-09-06 22:48:13 -0300

    :wrench: Fixes typo at Mailer config

--- config/prod.exs
+++ config/prod.exs
@@ -60,7 +60,7 @@ config :ex_commerce_web, ExCommerceWeb.Endpoint,
 
 config :ex_commerce, from_email: System.fetch_env!("EX_COMMERCE_FROM_EMAIL")
 
-config :ex_commerce, DjRumble.Mailer,
+config :ex_commerce, ExCommerce.Mailer,
   adapter: Bamboo.SendGridAdapter,
   api_key: System.get_env("SENDGRID_API_KEY"),
   hackney_opts: [
