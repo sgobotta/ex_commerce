b8c737d368a6da896278122f993e7f4538f608de
Santiago Botta <sgobotta@gmail.com>
2021-09-06 22:21:16 -0300

    :wrench: Updates prod config

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,9 @@ use Mix.Config
 config :ex_commerce_web, ExCommerceWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: {:system, "APP_HOST"}, port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: false
 
 # ## SSL Support
 #
