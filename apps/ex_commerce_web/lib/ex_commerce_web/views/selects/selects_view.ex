943fdb26cb4ac64cf34ca41abaf861c69023ccf2
Santiago Botta <sgobotta@gmail.com>
2021-09-22 21:19:16 -0300

    Implements a select view

---
+++ apps/ex_commerce_web/lib/ex_commerce_web/views/selects/selects_view.ex
@@ -0,0 +1,30 @@
+defmodule ExCommerceWeb.SelectsView do
+  @moduledoc false
+
+  use ExCommerceWeb, :view
+
+  def render("select", assigns) do
+    opts =
+      assigns
+      |> Map.put_new(:use_icon, true)
+      |> Map.put_new(:text, "Selected text")
+
+    opts =
+      case Map.get(opts, :use_icon) do
+        false ->
+          opts
+
+        true ->
+          class = """
+          flex items-center justify-center
+          font-semibold text-4xl
+          bg-gray-200 text-black
+          w-full h-full
+          """
+
+          Map.put_new(opts, :icon, content_tag(:span, "S", class: class))
+      end
+
+    render("select.html", opts)
+  end
+end
