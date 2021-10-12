3aefb25120fa4a9e003dfee6c817c79c7db9624f
Santiago Botta <sgobotta@gmail.com>
2021-10-12 20:41:38 -0300

    :computer: Creates some catalogue item variant fixtures

---
+++ apps/ex_commerce/test/support/fixtures/catalogue_item_variants_fixtures.ex
@@ -0,0 +1,46 @@
+defmodule ExCommerce.CatalogueItemVariantsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating entities via the
+  `ExCommerce.Offerings.CatalogueItemVariant` context.
+  """
+
+  alias ExCommerce.CatalogueItemsFixtures
+  alias ExCommerce.Offerings
+  alias ExCommerce.Offerings.{CatalogueItem, CatalogueItemVariant}
+
+  @valid_attrs %{
+    type: "some type",
+    price: Decimal.new("120.5")
+  }
+  @update_attrs %{
+    type: "some updated type",
+    price: Decimal.new("456.7")
+  }
+  @invalid_attrs %{type: nil, price: nil}
+
+  def valid_attrs(attrs \\ %{}), do: attrs |> Enum.into(@valid_attrs)
+  def update_attrs(attrs \\ %{}), do: attrs |> Enum.into(@update_attrs)
+  def invalid_attrs(attrs \\ %{}), do: attrs |> Enum.into(@invalid_attrs)
+
+  def create(attrs \\ %{}) do
+    attrs = assign_catalogue_item_maybe(attrs)
+
+    {:ok, %CatalogueItemVariant{} = catalogue_item_variant} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> Offerings.create_catalogue_item_variant()
+
+    catalogue_item_variant
+  end
+
+  defp assign_catalogue_item_maybe(attrs) do
+    case Map.has_key?(attrs, :catalogue_item_id) do
+      false ->
+        %CatalogueItem{id: catalogue_item_id} = CatalogueItemsFixtures.create()
+        Map.merge(attrs, %{catalogue_item_id: catalogue_item_id})
+
+      true ->
+        attrs
+    end
+  end
+end
