8b95b4f4058a69b3711737ca48d9a28f4616476a
Santiago Botta <sgobotta@gmail.com>
2021-12-20 18:09:37 -0300

    Updates model to build a variant price preview

--- apps/ex_commerce/lib/ex_commerce/offerings/catalogue_item_option.ex
+++ apps/ex_commerce/lib/ex_commerce/offerings/catalogue_item_option.ex
@@ -5,6 +5,8 @@ defmodule ExCommerce.Offerings.CatalogueItemOption do
   use Ecto.Schema
   import Ecto.Changeset
 
+  import Decimal
+
   alias ExCommerce.Offerings.{
     CatalogueItem,
     CatalogueItemVariant
@@ -12,6 +14,7 @@ defmodule ExCommerce.Offerings.CatalogueItemOption do
 
   @fields [:price_modifier, :is_visible]
   @foreign_fields [:brand_id, :catalogue_item_id, :catalogue_item_variant_id]
+  @virtual_fields [:delete, :price_preview]
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -21,6 +24,7 @@ defmodule ExCommerce.Offerings.CatalogueItemOption do
     field :brand_id, :binary_id
 
     # Virtual fields
+    field :price_preview, :decimal, virtual: true
     field :temp_id, :string, virtual: true
     field :delete, :boolean, virtual: true
 
@@ -42,12 +46,14 @@ defmodule ExCommerce.Offerings.CatalogueItemOption do
       :temp_id,
       catalogue_item_option.temp_id || attrs["temp_id"]
     )
-    |> cast(attrs, @fields ++ @foreign_fields ++ [:delete])
-    |> cast_assoc(:catalogue_item)
-    |> cast_assoc(:catalogue_item_variant)
+    |> cast(attrs, @fields ++ @foreign_fields ++ @virtual_fields)
+    # Removed to put structs in form params
+    # |> cast_assoc(:catalogue_item)
+    # |> cast_assoc(:catalogue_item_variant)
     |> validate_required(@fields ++ @foreign_fields)
     |> validate_number(:price_modifier, greater_than_or_equal_to: 0)
     |> maybe_mark_for_deletion()
+    |> maybe_build_price_preview(attrs)
   end
 
   defp maybe_mark_for_deletion(%{data: %{id: nil}} = changeset), do: changeset
@@ -58,5 +64,51 @@ defmodule ExCommerce.Offerings.CatalogueItemOption do
     else
       changeset
     end
+  end
+
+  defp preload_assocs(catalogue_item_option) do
+    ExCommerce.Repo.preload(catalogue_item_option, [:catalogue_item_variant])
+  end
+
+  defp maybe_build_price_preview(
+         %Ecto.Changeset{changes: %{price_modifier: price_modifier}} =
+           changeset,
+         %{"catalogue_item_variant" => %CatalogueItemVariant{price: price}}
+       )
+       when is_decimal(price_modifier) do
+    put_change(changeset, :price_preview, apply_discount(price, price_modifier))
+  end
+
+  defp maybe_build_price_preview(
+         %Ecto.Changeset{
+           data:
+             %__MODULE__{
+               price_modifier: price_modifier
+             } = catalogue_item_option
+         } = changeset,
+         _attrs
+       ) do
+    case preload_assocs(catalogue_item_option) do
+      %__MODULE__{
+        catalogue_item_variant: %CatalogueItemVariant{price: price}
+      } ->
+        put_change(
+          changeset,
+          :price_preview,
+          apply_discount(price, price_modifier)
+        )
+
+      %__MODULE__{catalogue_item_variant: _catalogue_item_variant} ->
+        changeset
+    end
+  end
+
+  defp maybe_build_price_preview(changeset, _attrs), do: changeset
+
+  defp apply_discount(price, price_modifier) do
+    Decimal.round(
+      Decimal.sub(price, Decimal.mult(price, Decimal.div(price_modifier, 100))),
+      2
+    )
   end
 end
