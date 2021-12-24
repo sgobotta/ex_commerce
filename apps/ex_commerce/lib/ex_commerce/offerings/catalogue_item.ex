6f84abcd0b2c03ff76384e55c380f19f124d71fa
Santiago Botta <sgobotta@gmail.com>
2021-12-24 14:40:34 -0300

    :computer: Updates changeset to handle associations

--- apps/ex_commerce/lib/ex_commerce/offerings/catalogue_item.ex
+++ apps/ex_commerce/lib/ex_commerce/offerings/catalogue_item.ex
@@ -7,6 +7,7 @@ defmodule ExCommerce.Offerings.CatalogueItem do
 
   import Ecto.Changeset
 
+  alias ExCommerce.Offerings
   alias ExCommerce.Offerings.{
     CatalogueItemOptionGroup,
     CatalogueItemOptionGroupItem,
@@ -27,7 +28,8 @@ defmodule ExCommerce.Offerings.CatalogueItem do
     has_many :variants, CatalogueItemVariant, on_delete: :delete_all
 
     many_to_many :option_groups, CatalogueItemOptionGroup,
-      join_through: CatalogueItemOptionGroupItem
+      join_through: CatalogueItemOptionGroupItem,
+      on_replace: :delete
 
     timestamps()
   end
@@ -37,7 +39,25 @@ defmodule ExCommerce.Offerings.CatalogueItem do
     catalogue_item
     |> cast(attrs, @fields ++ @foreign_fields)
     |> cast_assoc(:variants)
-    |> cast_assoc(:option_groups)
+    |> maybe_assoc_option_groups(attrs)
     |> validate_required(@fields ++ @foreign_fields)
   end
+
+  defp maybe_assoc_option_groups(
+         changeset,
+         %{"option_groups" => option_group_ids}
+       ) do
+    option_groups = Offerings.filter_catalogue_item_option_groups_by_id(
+      option_group_ids
+    )
+
+    put_assoc(changeset, :option_groups, option_groups)
+  end
+
+  defp maybe_assoc_option_groups(changeset, params)
+       when not is_map_key(params, "option_groups") do
+    put_assoc(changeset, :option_groups, [])
+  end
+
+  defp maybe_assoc_option_groups(changeset, _params), do: changeset
 end
