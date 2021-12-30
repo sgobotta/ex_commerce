798037cb63422989b28372f337d4dc75f4ea9749
Santiago Botta <sgobotta@gmail.com>
2021-12-29 21:34:29 -0300

    :computer: Creates an ex_commerce_assets app

---
+++ apps/ex_commerce_assets/lib/ex_commerce_assets.ex
@@ -0,0 +1,18 @@
+defmodule ExCommerceAssets do
+  @moduledoc """
+  Documentation for `ExCommerceAssets`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCommerceAssets.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/ex_commerce_assets/test/ex_commerce_assets_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCommerceAssetsTest do
+  use ExUnit.Case
+  doctest ExCommerceAssets
+
+  test "greets the world" do
+    assert ExCommerceAssets.hello() == :world
+  end
+end
