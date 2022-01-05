b7472a767f631a45f5a9b646a4b06ceadcc8946b
Santiago Botta <sgobotta@gmail.com>
2022-01-04 22:14:01 -0300

    :computer: Adds an upload helper function for Cloudex

--- apps/ex_commerce_assets/lib/ex_commerce_assets.ex
+++ apps/ex_commerce_assets/lib/ex_commerce_assets.ex
@@ -1,18 +1,29 @@
 defmodule ExCommerceAssets do
   @moduledoc """
-  Documentation for `ExCommerceAssets`.
+  `ExCommerceAssets` is a wrapper for assets uploading services.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExCommerceAssets.hello()
-      :world
-
+  Delegates to `Cloudex.upload/2`
   """
+  @spec upload([binary], map) :: list
+  def upload(files, options) do
+    Cloudex.upload(files, options)
+    |> parse_response()
+  end
+
   def hello do
     :world
+  end
+
+  defp parse_response(upload_response) when is_list(upload_response) do
+    Enum.map(upload_response, fn ok: %Cloudex.UploadedImage{} = ui ->
+      ui
+    end)
+  end
+
+  defp parse_response(upload_response) when is_tuple(upload_response) do
+    {:ok, %Cloudex.UploadedImage{} = ui} = upload_response
+    [ui]
   end
 end
