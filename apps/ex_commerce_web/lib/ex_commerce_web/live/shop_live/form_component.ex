28843442cba913861eebf159fff052e2c929afaf
Santiago Botta <sgobotta@gmail.com>
2022-01-08 04:59:44 -0300

    :computer: Adds images inputs and previews for the Shop forms

--- apps/ex_commerce_web/lib/ex_commerce_web/live/shop_live/form_component.ex
+++ apps/ex_commerce_web/lib/ex_commerce_web/live/shop_live/form_component.ex
@@ -5,13 +5,43 @@ defmodule ExCommerceWeb.ShopLive.FormComponent do
 
   use ExCommerceWeb, :live_component
 
-  alias ExCommerce.Marketplaces
-  alias ExCommerce.Marketplaces.Shop
+  use ExCommerceWeb.LiveFormHelpers, routes: Routes
+
+  alias ExCommerce.{Marketplaces, Photos}
+  alias ExCommerce.Marketplaces.{Brand, Shop}
+
+  import ExCommerceWeb.{
+    LiveFormHelpers,
+    Utils
+  }
 
   @app_host System.get_env("APP_HOST")
 
+  @uploads_path get_uploads_path()
+
   @impl true
-  def update(%{shop: shop} = assigns, socket) do
+  @spec mount(Phoenix.LiveView.Socket.t()) :: {:ok, Phoenix.LiveView.Socket.t()}
+  def mount(socket) do
+    {:ok,
+     socket
+     |> allow_upload(
+       :avatars,
+       accept: ~w(.jpg .jpeg .png),
+       max_entries: 1,
+       max_file_size: 2_000_000,
+       auto_upload: true
+     )
+     |> allow_upload(
+       :banners,
+       accept: ~w(.jpg .jpeg .png),
+       max_entries: 1,
+       max_file_size: 2_000_000,
+       auto_upload: true
+     )}
+  end
+
+  @impl true
+  def update(%{shop: %Shop{} = shop} = assigns, socket) do
     changeset = Marketplaces.change_shop(shop)
 
     {:ok,
@@ -37,8 +67,42 @@ defmodule ExCommerceWeb.ShopLive.FormComponent do
     save_shop(socket, socket.assigns.action, shop_params)
   end
 
+  def handle_event("cancel_avatar_entry", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :avatars, ref)}
+  end
+
+  def handle_event("cancel_banner_entry", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :banners, ref)}
+  end
+
+  # ----------------------------------------------------------------------------
+  # Private helpers
+  #
+
   defp save_shop(socket, :edit, shop_params) do
-    case Marketplaces.update_shop(socket.assigns.shop, shop_params) do
+    shop_params =
+      put_uploads(
+        socket,
+        shop_params,
+        &build_avatar_by_upload_entry/2,
+        &assign_avatars_param/2,
+        attr: :avatars
+      )
+
+    shop_params =
+      put_uploads(
+        socket,
+        shop_params,
+        &build_banner_by_upload_entry/2,
+        &assign_banners_param/2,
+        attr: :banners
+      )
+
+    case Marketplaces.update_shop(
+           socket.assigns.shop,
+           shop_params,
+           &consume_media_files(socket, &1)
+         ) do
       {:ok, %Shop{} = _shop} ->
         {:noreply,
          socket
@@ -52,9 +116,21 @@ defmodule ExCommerceWeb.ShopLive.FormComponent do
 
   defp save_shop(socket, :new, shop_params) do
     %{assigns: %{shop: %Shop{brand_id: brand_id}}} = socket
-    shop_params = Map.merge(shop_params, %{"brand_id" => brand_id})
 
-    case Marketplaces.create_shop(shop_params) do
+    shop_params =
+      put_uploads(
+        socket,
+        shop_params,
+        &build_avatar_by_upload_entry/2,
+        &assign_avatars_param/2,
+        attr: :avatars
+      )
+      |> assign_brand_id_param(brand_id)
+
+    case Marketplaces.create_shop(
+           shop_params,
+           &consume_media_files(socket, &1)
+         ) do
       {:ok, %Shop{} = _shop} ->
         {:noreply,
          socket
@@ -84,5 +160,83 @@ defmodule ExCommerceWeb.ShopLive.FormComponent do
       end
 
     assign(socket, :slug_placeholder, placeholder)
+  end
+
+  # ----------------------------------------------------------------------------
+  # File upload helpers
+  #
+
+  defp build_avatar_by_upload_entry(socket, entry) do
+    %{brand: %Brand{id: brand_id}} = socket.assigns
+    %{uuid: uuid} = entry
+    extension = ext(entry)
+
+    Photos.create_photo(%{
+      local_path: Routes.static_path(socket, "/uploads/#{uuid}.#{extension}"),
+      full_local_path: Path.join(@uploads_path, "#{uuid}.#{extension}"),
+      uuid: uuid,
+      brand_id: brand_id
+    })
+  end
+
+  defp assign_avatars_param(socket, new_avatars) do
+    %{shop: %Shop{avatars: avatars}} = socket.assigns
+
+    new_avatars ++ avatars
+  end
+
+  defp build_banner_by_upload_entry(socket, entry) do
+    %{brand: %Brand{id: brand_id}} = socket.assigns
+    %{uuid: uuid} = entry
+    extension = ext(entry)
+
+    Photos.create_photo(%{
+      local_path: Routes.static_path(socket, "/uploads/#{uuid}.#{extension}"),
+      full_local_path: Path.join(@uploads_path, "#{uuid}.#{extension}"),
+      uuid: uuid,
+      brand_id: brand_id
+    })
+  end
+
+  defp assign_banners_param(socket, new_banners) do
+    %{shop: %Shop{banners: banners}} = socket.assigns
+
+    new_banners ++ banners
+  end
+
+  defp consume_media_files(
+         socket,
+         %Shop{avatars: avatars, banners: banners} = shop
+       ) do
+    %{brand: %Brand{id: brand_id}} = socket.assigns
+    upload_opts = [folder: brand_id, tags: brand_id]
+
+    # Consumes uploads via the live form helper
+    {old_avatars, new_avatars} =
+      consume_uploads(
+        socket,
+        :avatars,
+        @uploads_path,
+        upload_opts,
+        avatars
+      )
+
+    {old_banners, new_banners} =
+      consume_uploads(
+        socket,
+        :banners,
+        @uploads_path,
+        upload_opts,
+        banners
+      )
+
+    # Saves new avatars and banners
+    {:ok, %Shop{} = shop} =
+      Marketplaces.update_shop(shop, %{
+        avatars: new_avatars ++ old_avatars,
+        banners: new_banners ++ old_banners
+      })
+
+    {:ok, shop}
   end
 end
