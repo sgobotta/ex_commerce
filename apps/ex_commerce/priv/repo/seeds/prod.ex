6a16d04e1f44a8a574f2f45eca99797f017ba3fa
Santiago Botta <sgobotta@gmail.com>
2022-01-08 17:03:04 -0300

    Creates some prod seeds

---
+++ apps/ex_commerce/priv/repo/seeds/prod.ex
@@ -0,0 +1,44 @@
+defmodule ExCommerce.Seeds.Prod do
+  @moduledoc """
+  Runs development fixtures.
+  """
+
+  require Logger
+
+  alias ExCommerce.Seeds.{
+    Brands,
+    BrandsUsers,
+    CatalogueCategories,
+    CatalogueItemOptionGroups,
+    CatalogueItemOptions,
+    CatalogueItems,
+    CatalogueItemVariants,
+    Catalogues,
+    Shops,
+    Users
+  }
+
+  @spec populate :: :ok
+  def populate do
+    # Removes debug messages in this run
+    :ok = Logger.configure(level: :info)
+
+    :ok = Logger.info("📌 Starting seeds population process...")
+
+    # Run seeds here
+    :ok = Users.populate()
+    :ok = Brands.populate()
+    :ok = BrandsUsers.populate()
+    :ok = Shops.populate()
+    :ok = Catalogues.populate()
+    :ok = CatalogueCategories.populate()
+    :ok = CatalogueItems.populate()
+    :ok = CatalogueItemVariants.populate()
+    :ok = CatalogueItemOptionGroups.populate()
+    :ok = CatalogueItemOptions.populate()
+
+    :ok = Logger.info("🌱 Succesfully created prod seeds.")
+
+    :ok
+  end
+end
