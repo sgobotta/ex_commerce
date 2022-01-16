47df6a159e1b04839976a17dac8aa7b2c9629780
Santiago Botta <sgobotta@gmail.com>
2022-01-16 20:44:39 -0300

    :computer: Implements a test helper module for uploads

---
+++ apps/ex_commerce_web/test/support/uploads_case.ex
@@ -0,0 +1,61 @@
+defmodule ExCommerceWeb.UploadsCase do
+  @moduledoc """
+  This module defines test case helpers to be used for uploads
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import Plug.Conn
+      import Phoenix.LiveViewTest
+
+      def valid_uploads_metadata do
+        [
+          %{
+            last_modified: 1_594_171_879_000,
+            name: "avatar-512x512.png",
+            content:
+              File.read!(
+                Path.join(
+                  Application.app_dir(:ex_commerce_web, "priv/static/images"),
+                  "/placeholders/avatar-512x512.png"
+                )
+              ),
+            type: "image/png"
+          }
+        ]
+      end
+
+      def upload_photos(
+            view,
+            form,
+            form_attr,
+            uploads_metadata,
+            cancel_event_name
+          ) do
+        photos = file_input(view, form, form_attr, uploads_metadata)
+
+        view =
+          Enum.reduce(uploads_metadata, view, fn %{name: name}, view ->
+            render_upload(photos, name, 100)
+          end)
+
+        for %{name: name} <- uploads_metadata do
+          assert view =~
+                   "<p class=\"text-base font-mono truncate\">\n#{name}\n    </p>"
+
+          assert view =~
+                   "<a class=\"mini-cancel-button\" href=\"#\" phx-click=\"#{
+                     cancel_event_name
+                   }\""
+
+          assert view =~
+                   "\n      &amp;times\n    </a></div><div class=\"\n    col-span-8 sm:col-span-4 row-span-1\n    px-5 sm:px-0\n    sm:text-center\n  \"><progress max=\"100\" value=\"100\"></progress>"
+        end
+
+        :ok
+      end
+    end
+  end
+end
