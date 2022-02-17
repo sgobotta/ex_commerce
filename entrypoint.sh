2cf01843b21cb2dce5e4a47fd2b221e888350685
Santiago Botta <sgobotta@gmail.com>
2022-02-17 00:16:34 -0300

    :wrench: Updates entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-/app/bin/ex_commerce eval ExCommerce.Release.create_db
-/app/bin/ex_commerce eval ExCommerce.Release.migrate
-/app/bin/ex_commerce start
+bin="/app/bin/ex_commerce"
+
+# Create db if not exists
+echo "Attempting to create db..."
+eval "$bin eval \"ExCommerce.Release.create_db\""
+
+# Migrate the database
+echo "Starting Migrations..."
+eval "$bin eval \"ExCommerce.Release.migrate\""
+
+# Start the elixir application
+echo "Starting Application..."
+exec "$bin" "start"
