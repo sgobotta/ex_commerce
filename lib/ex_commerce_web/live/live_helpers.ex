205a065bf5ad57526626f7b60167f65ad2c4ce3d
Santiago Botta <sgobotta@gmail.com>
2022-03-01 19:32:38 -0300

    :computer: Implements some views for the admin layout

--- lib/ex_commerce_web/live/live_helpers.ex
+++ lib/ex_commerce_web/live/live_helpers.ex
@@ -93,7 +93,7 @@ defmodule ExCommerceWeb.LiveHelpers do
   end
 
   # ----------------------------------------------------------------------------
-  # Select helpers
+  # Image helpers
   #
 
   @doc """
@@ -246,5 +246,378 @@ defmodule ExCommerceWeb.LiveHelpers do
     js
     |> JS.hide(to: "#modal", transition: "fade-out")
     |> JS.hide(to: "#modal-content", transition: "fade-out-scale")
+  end
+
+  # ----------------------------------------------------------------------------
+  # Menu revamp helpers
+  #
+
+  def connection_status(assigns) do
+    ~H"""
+    <div
+      id="connection-status"
+      class="
+        hidden
+        rounded-md
+        bg-red-50
+        p-4
+        fixed top-1 right-1 w-96
+        fade-in-scale
+        z-50
+      "
+      js-show={show("#connection-status")}
+      js-hide={hide("#connection-status")}
+    >
+      <div class="flex">
+        <div class="flex-shrink-0">
+          <svg
+            class="
+              animate-spin
+              -ml-1 mr-3 h-5 w-5
+              text-red-800
+            "
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+            </path>
+          </svg>
+        </div>
+        <div class="ml-3">
+          <p
+            class="text-sm font-medium text-red-800"
+            role="alert"
+          >
+            <%= render_slot(@inner_block) %>
+          </p>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  @doc """
+  Returns a button triggered dropdown with aria keyboard and focus supporrt.
+
+  Accepts the follow slots:
+
+    * `:id` - The id to uniquely identify this dropdown
+    * `:img` - The optional img to show beside the button title
+    * `:title` - The button title
+    * `:subtitle` - The button subtitle
+
+  ## Examples
+
+      <.dropdown id={@id}>
+        <:img src={@current_user.avatar_url}/>
+        <:title><%= @current_user.name %></:title>
+        <:subtitle>@<%= @current_user.username %></:subtitle>
+
+        <:link navigate={profile_path(@current_user)}>View Profile</:link>
+        <:link navigate={Routes.settings_path(LiveBeatsWeb.Endpoint, :edit)}Settings</:link>
+      </.dropdown>
+  """
+  def dropdown(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:img, fn -> nil end)
+      |> assign_new(:title, fn -> nil end)
+      |> assign_new(:subtitle, fn -> nil end)
+
+    ~H"""
+    <!-- User account dropdown -->
+    <div class="px-3 mt-6 relative inline-block text-left">
+      <div>
+        <button
+          id={@id}
+          type="button"
+          class="
+            group
+            w-full
+            rounded-md
+            px-3.5 py-2
+            text-sm text-left font-medium text-gray-700
+            bg-gray-100
+            hover:bg-gray-200
+            focus:outline-none focus:ring-2 focus:ring-offset-2
+            focus:ring-offset-gray-100 focus:ring-purple-500
+          "
+          phx-click={show_dropdown("##{@id}-dropdown")}
+          phx-hook="Menu"
+          data-active-class="bg-gray-100"
+          aria-haspopup="true"
+        >
+          <span class="flex w-full justify-between items-center">
+            <span class="flex min-w-0 items-center justify-between space-x-3">
+              <%= for img <- @img do %>
+                <img
+                  class="
+                    w-10 h-10
+                    bg-gray-300
+                    rounded-full
+                    flex-shrink-0
+                  "
+                  alt=""
+                  {assigns_to_attributes(img)}
+                />
+              <% end %>
+              <span class="flex-1 flex flex-col min-w-0">
+                <span class="text-gray-900 text-sm font-medium truncate">
+                  <%= render_slot(@title) %>
+                </span>
+                <span class="text-gray-500 text-sm truncate">
+                  <%= render_slot(@subtitle) %>
+                </span>
+              </span>
+            </span>
+            <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+              fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd"
+                d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                clip-rule="evenodd"></path>
+            </svg>
+          </span>
+        </button>
+      </div>
+      <div
+        id={"#{@id}-dropdown"}
+        phx-click-away={hide_dropdown("##{@id}-dropdown")}
+        class="
+          hidden z-10 mx-3 origin-top
+          absolute right-0 left-0
+          mt-1
+          rounded-md
+          shadow-lg
+          bg-white
+          ring-1 ring-black ring-opacity-5
+          divide-y divide-gray-200
+        "
+        role="menu"
+        aria-labelledby={@id}
+      >
+        <div class="py-1" role="none">
+          <%= for link <- @link do %>
+            <.link
+              tabindex="-1"
+              role="menuitem"
+              class="
+                block
+                px-4 py-2
+                text-sm text-gray-700
+                hover:bg-gray-100
+                focus:outline-none focus:ring-2 focus:ring-offset-2
+                focus:ring-offset-gray-100 focus:ring-purple-500
+              "
+              {link}
+            ><%= render_slot(link) %></.link>
+          <% end %>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  def show_mobile_sidebar(js \\ %JS{}) do
+    js
+    |> JS.show(to: "#mobile-sidebar-container", transition: "fade-in")
+    |> JS.show(
+      to: "#mobile-sidebar",
+      display: "flex",
+      time: 300,
+      transition:
+        {"transition ease-in-out duration-300 transform", "-translate-x-full",
+         "translate-x-0"}
+    )
+    |> JS.hide(to: "#show-mobile-sidebar", transition: "fade-out")
+    |> JS.dispatch("js:exec",
+      to: "#hide-mobile-sidebar",
+      detail: %{call: "focus", args: []}
+    )
+  end
+
+  def hide_mobile_sidebar(js \\ %JS{}) do
+    js
+    |> JS.hide(to: "#mobile-sidebar-container", transition: "fade-out")
+    |> JS.hide(
+      to: "#mobile-sidebar",
+      time: 300,
+      transition:
+        {"transition ease-in-out duration-300 transform", "translate-x-0",
+         "-translate-x-full"}
+    )
+    |> JS.show(to: "#show-mobile-sidebar", transition: "fade-in")
+    |> JS.dispatch("js:exec",
+      to: "#show-mobile-sidebar",
+      detail: %{call: "focus", args: []}
+    )
+  end
+
+  def show(js \\ %JS{}, selector) do
+    JS.show(js,
+      to: selector,
+      time: 300,
+      display: "inline-block",
+      transition:
+        {"ease-out duration-300",
+         "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95",
+         "opacity-100 translate-y-0 sm:scale-100"}
+    )
+  end
+
+  def hide(js \\ %JS{}, selector) do
+    JS.hide(js,
+      to: selector,
+      time: 300,
+      transition:
+        {"transition ease-in duration-300", "transform opacity-100 scale-100",
+         "transform opacity-0 scale-95"}
+    )
+  end
+
+  def show_dropdown(to) do
+    JS.show(
+      to: to,
+      transition:
+        {"transition ease-out duration-120", "transform opacity-0 scale-95",
+         "transform opacity-100 scale-100"}
+    )
+    |> JS.set_attribute({"aria-expanded", "true"}, to: to)
+  end
+
+  def hide_dropdown(to) do
+    JS.hide(
+      to: to,
+      transition:
+        {"transition ease-in duration-120", "transform opacity-100 scale-100",
+         "transform opacity-0 scale-95"}
+    )
+    |> JS.remove_attribute("aria-expanded", to: to)
+  end
+
+  def link(%{navigate: _to} = assigns) do
+    assigns = assign_new(assigns, :class, fn -> nil end)
+
+    ~H"""
+    <a
+      href={@navigate}
+      data-phx-link="redirect"
+      data-phx-link-state="push"
+      class={@class}
+    >
+      <%= render_slot(@inner_block) %>
+    </a>
+    """
+  end
+
+  def link(%{patch: to} = assigns) do
+    opts = assigns |> assigns_to_attributes() |> Keyword.put(:to, to)
+    assigns = assign(assigns, :opts, opts)
+
+    ~H"""
+    <%= live_patch @opts do %><%= render_slot(@inner_block) %><% end %>
+    """
+  end
+
+  def link(%{} = assigns) do
+    opts =
+      assigns
+      |> assigns_to_attributes()
+      |> Keyword.put(:to, assigns[:href] || "#")
+
+    assigns = assign(assigns, :opts, opts)
+
+    ~H"""
+    <%= Phoenix.HTML.Link.link @opts do %><%= render_slot(@inner_block) %><% end %>
+    """
+  end
+
+  def icon(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:outlined, fn -> false end)
+      |> assign_new(:class, fn -> "w-4 h-4 inline-block" end)
+      |> assign_new(:"aria-hidden", fn ->
+        !Map.has_key?(assigns, :"aria-label")
+      end)
+
+    ~H"""
+    <%= if @outlined do %>
+      <%= apply(Heroicons.Outline, @name, [assigns_to_attributes(assigns, [:outlined, :name])]) %>
+    <% else %>
+      <%= apply(Heroicons.Solid, @name, [assigns_to_attributes(assigns, [:outlined, :name])]) %>
+    <% end %>
+    """
+  end
+
+  def flash(%{kind: :error} = assigns) do
+    ~H"""
+    <%= if live_flash(@flash, @kind) do %>
+      <div
+        id="flash"
+        class="rounded-md bg-red-50 p-4 fixed top-1 right-1 w-96 fade-in-scale z-50"
+        phx-click={JS.push("lv:clear-flash") |> JS.remove_class("fade-in-scale", to: "#flash") |> hide("#flash")}
+        phx-hook="Flash"
+      >
+        <div class="flex justify-between items-center space-x-3 text-red-700">
+          <.icon name={:exclamation_circle} class="w-5 w-5"/>
+          <p class="flex-1 text-sm font-medium" role="alert">
+            <%= live_flash(@flash, @kind) %>
+          </p>
+          <button type="button" class="inline-flex bg-red-50 rounded-md p-1.5 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-50 focus:ring-red-600">
+            <.icon name={:x} class="w-4 h-4" />
+          </button>
+        </div>
+      </div>
+    <% end %>
+    """
+  end
+
+  def flash(%{kind: :info} = assigns) do
+    ~H"""
+    <%= if live_flash(@flash, @kind) do %>
+      <div
+        id="flash"
+        class="rounded-md bg-green-50 p-4 fixed top-1 right-1 w-96 fade-in-scale z-50"
+        phx-click={JS.push("lv:clear-flash") |> JS.remove_class("fade-in-scale") |> hide("#flash")}
+        phx-value-key="info"
+        phx-hook="Flash"
+      >
+        <div class="flex justify-between items-center space-x-3 text-green-700">
+          <.icon name={:check_circle} class="w-5 h-5"/>
+          <p class="flex-1 text-sm font-medium" role="alert">
+            <%= live_flash(@flash, @kind) %>
+          </p>
+          <button
+            type="button"
+            class="
+              inline-flex
+              rounded-md p-1.5
+              text-green-500
+              bg-green-50
+              hover:bg-green-100
+              focus:outline-none focus:ring-2 focus:ring-offset-2
+              focus:ring-offset-green-50 focus:ring-green-600
+            "
+          >
+            <.icon name={:x} class="w-4 h-4" />
+          </button>
+        </div>
+      </div>
+    <% end %>
+    """
   end
 end
--- lib/ex_commerce_web/views/layout_view.ex
+++ lib/ex_commerce_web/views/layout_view.ex
@@ -1,3 +1,63 @@
 defmodule ExCommerceWeb.LayoutView do
   use ExCommerceWeb, :view
+
+  # Phoenix LiveDashboard is available only in development by default,
+  # so we instruct Elixir to not warn if the dashboard route is missing.
+  @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  alias ExCommerceWeb.Endpoint
+
+  def sidebar_account_dropdown(assigns) do
+    ~H"""
+    <.dropdown id={@id}>
+      <:img src={"https://images.unsplash.com/photo-1610397095767-84a5b4736cbd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"}/>
+      <:title>User Name</:title>
+      <:subtitle>@<%= @current_user.email %></:subtitle>
+
+      <:link href={Routes.user_session_path(Endpoint, :delete)} method={:delete}>Sign out</:link>
+    </.dropdown>
+    """
+  end
+
+  def sidebar_nav_links(assigns) do
+    ~H"""
+    <div class="space-y-1">
+      <%= if @current_user do %>
+        <.link
+          navigate={Routes.brand_index_path(Endpoint, :index)}
+          class={
+            "text-gray-700 hover:text-gray-900 group flex items-center
+            px-2 py-2
+            text-sm font-medium
+            rounded-md
+            #{if @active_tab == :settings, do: "bg-gray-200", else: "hover:bg-gray-50"}
+          "}
+          aria-current={if @active_tab == :settings, do: "true", else: "false"}
+        >
+          <.icon name={:adjustments} outlined class="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0 h-6 w-6"/>
+          Brands
+        </.link>
+      <% else %>
+        <.link navigate={Routes.user_session_path(Endpoint, :new)}
+          class="
+            text-gray-700 hover:text-gray-900
+            hover:bg-gray-50
+            group flex items-center
+            px-2 py-2
+            text-sm font-medium
+            rounded-md
+          "
+        >
+          <svg class="text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0 h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+          </svg>
+          Sign in
+        </.link>
+      <% end %>
+    </div>
+    """
+  end
 end
