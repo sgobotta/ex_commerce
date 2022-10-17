11887708047189df1a1037c517778a3199df89ff
Wasabi Brownies <makesmereal@gmail.com>
2022-10-17 12:27:04 -0300

    :computer: Upgrade the entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,62 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 bin="bin/ex_commerce"
 
-# Create db if not exists
-# echo "Attempting to create db..."
-# eval "$bin eval \"ExCommerce.Release.create_db\""
+hello() {
+  set -e
 
-# Migrate the database
-echo "Starting Migrations..."
-eval "$bin eval \"ExCommerce.Release.migrate\""
+  # Simple application interaction
+ $bin eval "IO.puts(ExCommerce.hello())"
+}
 
-# Start the elixir application
-echo "Starting Application..."
-exec "$bin" "start"
+create_db() {
+  set -e
+
+  echo ======== Create DB ========
+  $bin eval "ExCommerce.Release.create_db()"
+}
+
+migrate() {
+  set -e
+
+  echo ======== Starting ecto migration ========
+  $bin eval "ExCommerce.Release.migrate()"
+}
+
+seeds() {
+  set -e
+
+  echo ======== Creating seeds ========
+  $bin eval "ExCommerce.Release.seed()"
+}
+
+setup_db() {
+  set -e
+
+  echo Setting up DB...
+  # Run the create db
+  create_db
+  # Run the migrate script
+  migrate
+  # Run seeds creation
+  seeds
+}
+
+start() {
+  set -e
+
+  # Run the setup_db script
+  setup_db
+
+  echo ======== Starting ex_commerce ========
+  $bin start
+}
+
+case $1 in
+  hello) "$@"; exit;;
+  create_db) "$@"; exit;;
+  migrate) "$@"; exit;;
+  seeds) "$@"; exit;;
+  setup_db) "$@"; exit;;
+  start) "$@"; exit;;
+esac
