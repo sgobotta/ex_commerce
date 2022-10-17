e008c0cf0dbae4bdebf1a999c1dcb521aa1a55c5
Wasabi Brownies <makesmereal@gmail.com>
2022-10-17 12:14:42 -0300

    :computer: Add a function for testing releases

--- lib/ex_commerce.ex
+++ lib/ex_commerce.ex
@@ -6,4 +6,6 @@ defmodule ExCommerce do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def hello, do: :world
 end
