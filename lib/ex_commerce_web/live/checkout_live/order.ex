fd6d44b5f696aebd1010134ea88ae9c46be4793a
Wasabi Brownies <makesmereal@gmail.com>
2022-10-18 14:17:13 -0300

    :computer: Update order details on Order checkout step

--- lib/ex_commerce_web/live/checkout_live/order.ex
+++ lib/ex_commerce_web/live/checkout_live/order.ex
@@ -13,12 +13,14 @@ defmodule ExCommerceWeb.CheckoutLive.Order do
   alias ExCommerce.Checkout
   alias ExCommerce.Checkout.{Cart, Order}
 
+  alias ExCommerce.Marketplaces.Shop
+
+  alias ExCommerce.Offerings.Catalogue
+
   alias ExCommerceWeb.CheckoutLive.Components
 
   @impl true
   def mount(params, session, socket) do
-    %{assigns: %{cart: %Cart{order: %Order{} = order}}} = socket
-
     {
       :ok,
       socket
@@ -27,7 +29,6 @@ defmodule ExCommerceWeb.CheckoutLive.Order do
       |> assign(:cart_visible, true)
       |> assign(:brand_slug, params["brand"])
       |> assign(:shop_slug, params["shop"])
-      |> assign(:changeset, Checkout.change_order(order))
     }
   end
 
@@ -36,8 +37,8 @@ defmodule ExCommerceWeb.CheckoutLive.Order do
     do: {:noreply, apply_action(socket, socket.assigns.live_action, params)}
 
   @impl true
-  def handle_event("validate", _params, socket) do
-    {:noreply, socket}
+  def handle_event("validate", %{"order" => order_params}, socket) do
+    {:noreply, assign_changeset(socket, order_params)}
   end
 
   def handle_event("save", _params, socket) do
@@ -65,6 +66,8 @@ defmodule ExCommerceWeb.CheckoutLive.Order do
         catalogue_id
       )
     )
+    |> assign_catalogue(catalogue_id)
+    |> assign_changeset()
     |> assign_cart_path(brand_slug, shop_slug, catalogue_id)
     |> assign_nav_title()
   end
@@ -93,6 +96,30 @@ defmodule ExCommerceWeb.CheckoutLive.Order do
     |> assign_catalogue(catalogue_id)
     |> assign(:cart_path, "#")
     |> assign_nav_title()
+  end
+
+  defp assign_changeset(socket, params \\ %{}) do
+    %{
+      cart: %Cart{order: %Order{} = order} = cart,
+      catalogue: %Catalogue{id: catalogue_id},
+      shop: %Shop{id: shop_id, brand_id: brand_id}
+    } = socket.assigns
+
+    params =
+      %{
+        "brand_id" => brand_id,
+        "catalogue_id" => catalogue_id,
+        "shop_id" => shop_id
+      }
+      |> Map.merge(params)
+
+    %Ecto.Changeset{} = changeset = Checkout.change_order(order, params)
+
+    %Cart{} = cart = Checkout.update_cart_order(cart, changeset)
+
+    socket
+    |> assign(:changeset, changeset)
+    |> assign(:cart, cart)
   end
 
   defp assign_catalogue(socket, catalogue_id),
