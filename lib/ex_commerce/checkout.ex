040d14948a19a402e65745115a1f515b920df110
Wasabi Brownies <makesmereal@gmail.com>
2022-10-21 13:53:44 -0300

    :computer: Implement some basic functions to parse orders into message strings

--- lib/ex_commerce/checkout.ex
+++ lib/ex_commerce/checkout.ex
@@ -5,8 +5,17 @@ defmodule ExCommerce.Checkout do
 
   import Ecto.Query, warn: false
 
+  alias ExCommerce.Offerings.{
+    CatalogueItem,
+    CatalogueItemOption,
+    CatalogueItemOptionGroup,
+    CatalogueItemVariant
+  }
+
   alias ExCommerce.Checkout.{Cart, Order, OrderItem, Supervisor}
   alias ExCommerce.Repo
+
+  import ExCommerceNumeric
 
   defdelegate child_spec(init_arg), to: Supervisor
 
@@ -73,6 +82,160 @@ defmodule ExCommerce.Checkout do
     order_items
     |> Enum.reduce(0, fn %OrderItem{price: price}, acc ->
       ExCommerceNumeric.add(acc, price)
+    end)
+  end
+
+  def get_order_message(%Cart{} = cart) do
+    price = get_order_price(cart)
+
+    %Order{address: address, order_items: order_items, note: note} =
+      _order = Cart.get_order(cart)
+
+    order_items =
+      Enum.reduce(order_items, "", fn %OrderItem{
+                                        catalogue_item: %CatalogueItem{
+                                          name: name
+                                        },
+                                        price: order_item_price,
+                                        quantity: quantity,
+                                        variant: %CatalogueItemVariant{
+                                          type: type,
+                                          price: variant_price
+                                        }
+                                      } = order_item,
+                                      acc ->
+        option_groups_message = get_option_groups_message(order_item)
+
+        acc <>
+          """
+          ▪ (*#{quantity}*) #{name}, #{type} ... $#{variant_price}* (u) $#{order_item_price}*%0A
+          #{option_groups_message}
+          """
+      end)
+
+    text = """
+    PEDIDO: *#{Ecto.UUID.generate()}*%0A
+    %0A
+    #{order_items}
+    %0A
+    💵 Total: *$#{price}*%0A
+
+    🏠 Dirección: *#{address}*%0A
+    📝 Notas: _#{note}_
+    """
+
+    text
+  end
+
+  defp get_option_groups_message(%OrderItem{
+         available_option_groups: %{
+           values: available_option_groups
+         },
+         option_groups: option_groups
+       }) do
+    Enum.reduce(option_groups, "", fn
+      {_catalogue_option_group, %{"value" => []}}, acc ->
+        acc
+
+      {_catalogue_option_group, %{"value" => ""}}, acc ->
+        acc
+
+      {catalogue_option_group, %{"value" => values}}, acc
+      when is_list(values) ->
+        %CatalogueItemOptionGroup{
+          name: group_name,
+          options: options
+        } =
+          _catalogue_item_option_group =
+          Enum.find(available_option_groups, fn %CatalogueItemOptionGroup{
+                                                  id: id
+                                                } ->
+            id == catalogue_option_group
+          end)
+
+        options =
+          Enum.filter(options, fn %CatalogueItemOption{id: id} ->
+            id in values
+          end)
+
+        acc =
+          acc <>
+            """
+            %20%20*#{group_name}*:%0A
+            """
+
+        Enum.reduce(options, acc, fn %CatalogueItemOption{
+                                       catalogue_item_variant:
+                                         %CatalogueItemVariant{
+                                           catalogue_item: %CatalogueItem{
+                                             name: catalogue_item_name
+                                           },
+                                           price: variant_price,
+                                           type: variant_type
+                                         },
+                                       price_modifier: price_modifier
+                                     },
+                                     acc ->
+          discount_price =
+            format_price(
+              Decimal.sub(
+                variant_price,
+                Decimal.mult(
+                  variant_price,
+                  Decimal.div(price_modifier, 100)
+                )
+              )
+            )
+
+          acc <>
+            """
+            %20%20%20%20▫ #{catalogue_item_name}, #{variant_type} ... ~$#{variant_price}~ $#{discount_price}%0A
+            """
+        end)
+
+      {catalogue_option_group, %{"value" => value}}, acc
+      when is_binary(value) ->
+        %CatalogueItemOptionGroup{
+          name: group_name,
+          options: options
+        } =
+          _catalogue_item_option_group =
+          Enum.find(available_option_groups, fn %CatalogueItemOptionGroup{
+                                                  id: id
+                                                } ->
+            id == catalogue_option_group
+          end)
+
+        %CatalogueItemOption{
+          catalogue_item_variant: %CatalogueItemVariant{
+            catalogue_item: %CatalogueItem{
+              name: catalogue_item_name
+            },
+            price: variant_price,
+            type: variant_type
+          },
+          price_modifier: price_modifier
+        } =
+          Enum.find(options, fn %CatalogueItemOption{id: id} ->
+            id == value
+          end)
+
+        discount_price =
+          format_price(
+            Decimal.sub(
+              variant_price,
+              Decimal.mult(
+                variant_price,
+                Decimal.div(price_modifier, 100)
+              )
+            )
+          )
+
+        acc <>
+          """
+          %20%20*#{group_name}*:%0A
+          %20%20%20%20▫ #{catalogue_item_name}, #{variant_type} ... ~$#{variant_price}~ $#{discount_price}%0A
+          """
     end)
   end
 
