3223b3eff249b5973b4931ae17af2e69358fd6e7
Santiago Botta <sgobotta@gmail.com>
2022-12-29 18:42:32 -0300

    :computer: Remove unused code

--- test/support/fixtures/shops_fixtures.ex
+++ test/support/fixtures/shops_fixtures.ex
@@ -4,9 +4,8 @@ defmodule ExCommerce.ShopsFixtures do
   `ExCommerce.Marketplaces.Brand` context.
   """
 
-  alias ExCommerce.BrandsFixtures
   alias ExCommerce.Marketplaces
-  alias ExCommerce.Marketplaces.{Brand, Shop}
+  alias ExCommerce.Marketplaces.Shop
 
   import ExCommerce.FixtureHelpers
 
