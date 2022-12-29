79acf973d49715e047683d9bec2d05baa357695d
Santiago Botta <sgobotta@gmail.com>
2022-12-29 16:51:04 -0300

    :test_tube: Update Order tests

--- test/ex_commerce/checkout_test.exs
+++ test/ex_commerce/checkout_test.exs
@@ -3,6 +3,7 @@ defmodule ExCommerce.CheckoutTest do
   use ExCommerce.DataCase
 
   alias ExCommerce.Checkout
+  alias ExCommerce.Checkout.OrderItem
 
   require Decimal
 
@@ -27,7 +28,7 @@ defmodule ExCommerce.CheckoutTest do
       Offerings
     }
 
-    test "add_to_order/2 adds an order item to a new order" do
+    setup do
       %Catalogue{
         id: catalogue_id,
         brand_id: brand_id
@@ -91,124 +92,146 @@ defmodule ExCommerce.CheckoutTest do
           }
         )
 
+      %{cart: cart, order_item: order_item}
+    end
+
+    @tag :wip
+    test "add_to_order/2 adds an order item to a new order", %{
+      cart: %Cart{} = cart,
+      order_item: %Ecto.Changeset{} = order_item
+    } do
+      # Setup
       %Cart{server: server} = Checkout.add_to_order(cart, order_item)
 
+      # Exercise
       %Cart.Order{order_items: order_items} = CartServer.get_order(server)
 
+      # Verify
       assert length(order_items) == 1
-      %Cart.OrderItem{} = order_item = Enum.at(order_items, 0)
-      assert order_item.quantity == 2
-      assert order_item.catalogue_item_id == catalogue_item_id
-      assert Decimal.is_decimal(order_item.price)
-      assert order_item.variant_id == variant_id
+
+      %Cart.OrderItem{quantity: quantity, price: price} =
+        ^order_item = Enum.at(order_items, 0)
+
+      assert quantity == 2
+      assert Decimal.is_decimal(price)
+    end
+
+    test "from_cart_order/1 returns an #{Order} struct" do
     end
   end
 
-  # describe "orders" do
-  #   alias ExCommerce.{
-  #     BrandsFixtures,
-  #     CataloguesFixtures,
-  #     OrderFixtures,
-  #     ShopsFixtures
-  #   }
+  describe "orders" do
+    alias ExCommerce.{
+      BrandsFixtures,
+      CataloguesFixtures,
+      ShopsFixtures
+    }
 
-  #   alias ExCommerce.Checkout.Order
-  #   alias ExCommerce.Marketplaces.{Brand, Shop}
-  #   alias ExCommerce.Offerings.Catalogue
+    alias ExCommerce.Checkout.{Order, OrderFixtures, OrderItemFixtures}
+    alias ExCommerce.Marketplaces.{Brand, Shop}
+    alias ExCommerce.Offerings.Catalogue
 
-  #   @valid_attrs OrderFixtures.valid_attrs()
+    @valid_attrs OrderFixtures.valid_attrs()
+    @update_attrs OrderFixtures.update_attrs()
+    @invalid_attrs OrderFixtures.invalid_attrs()
 
-  #   @invalid_attrs OrderFixtures.invalid_attrs()
+    setup do
+      %Brand{id: brand_id} = brand = BrandsFixtures.create()
 
-  #   setup do
-  #     %Brand{id: brand_id} = brand = BrandsFixtures.create()
+      %{
+        brand: brand,
+        catalogue: CataloguesFixtures.create(%{brand_id: brand_id}),
+        shop: ShopsFixtures.create(%{brand_id: brand_id})
+      }
+    end
 
-  #     %{
-  #       brand: brand,
-  #       catalogue: CataloguesFixtures.create(%{brand_id: brand_id}),
-  #       shop: ShopsFixtures.create(%{brand_id: brand_id})
-  #     }
-  #   end
+    @tag :wip
+    test "list_orders/0 returns all orders" do
+      %Order{} = order = OrderFixtures.create()
+      assert Checkout.list_orders() == [order]
+    end
 
-  #   @tag :skip
-  #   test "list_orders/0 returns all orders" do
-  #     %Order{} = order = OrderFixtures.create()
-  #     assert Checkout.list_orders() == [order]
-  #   end
+    @tag :wip
+    test "get_order!/1 returns the order with given id" do
+      %Order{id: order_id} = order = OrderFixtures.create()
+      assert Checkout.get_order!(order_id) == order
+    end
 
-  #   @tag :skip
-  #   test "get_order!/1 returns the order with given id" do
-  #     %Order{id: order_id} = order = OrderFixtures.create()
-  #     assert Checkout.get_order!(order_id) == order
-  #   end
+    @tag :wip
+    test "create_order/1 with valid data creates an order", %{
+      brand: %Brand{id: brand_id},
+      catalogue: %Catalogue{id: catalogue_id},
+      shop: %Shop{id: shop_id}
+    } do
+      valid_attrs =
+        Map.merge(@valid_attrs, %{
+          brand_id: brand_id,
+          catalogue_id: catalogue_id,
+          shop_id: shop_id
+        })
 
-  #   @tag :skip
-  #   test "create_order/1 with valid data creates a order", %{
-  #     brand: %Brand{id: brand_id},
-  #     catalogue: %Catalogue{id: catalogue_id},
-  #     shop: %Shop{id: shop_id}
-  #   } do
-  #     valid_attrs =
-  #       Map.merge(@valid_attrs, %{
-  #         brand_id: brand_id,
-  #         catalogue_id: catalogue_id,
-  #         shop_id: shop_id
-  #       })
+      assert {:ok,
+              %Order{
+                brand_id: ^brand_id,
+                catalogue_id: ^catalogue_id,
+                shop_id: ^shop_id
+              }} = Checkout.create_order(valid_attrs)
+    end
 
-  #     assert {:ok,
-  #             %Order{
-  #               brand_id: ^brand_id,
-  #               catalogue_id: ^catalogue_id,
-  #               shop_id: ^shop_id
-  #             }} = Checkout.create_order(valid_attrs)
-  #   end
+    @tag :wip
+    test "create_order/1 with order_items creates an order", %{
+      brand: %Brand{id: brand_id},
+      catalogue: %Catalogue{id: catalogue_id},
+      shop: %Shop{id: shop_id}
+    } do
+      valid_attrs =
+        Map.merge(@valid_attrs, %{
+          brand_id: brand_id,
+          catalogue_id: catalogue_id,
+          shop_id: shop_id
+        })
 
-  #   @tag :skip
-  #   test "create_order/1 with invalid data returns error changeset" do
-  #     assert {:error, %Ecto.Changeset{}} =
-  #       Checkout.create_order(@invalid_attrs)
-  #   end
+      assert {:ok,
+              %Order{
+                brand_id: ^brand_id,
+                catalogue_id: ^catalogue_id,
+                shop_id: ^shop_id
+              }} = Checkout.create_order(valid_attrs)
+    end
 
-  #   @tag :skip
-  #   test "update_order/2 with valid data updates the order" do
-  #     %Order{} = order = OrderFixtures.create()
-  #     update_attrs = %{}
+    @tag :wip
+    test "create_order/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Checkout.create_order(@invalid_attrs)
+    end
 
-  #     assert {:ok, %Order{}} = Checkout.update_order(order, update_attrs)
-  #   end
+    @tag :wip
+    test "update_order/2 with valid data updates the order" do
+      %Order{} = order = OrderFixtures.create()
 
-  #   @tag :skip
-  #   test "update_order/2 with invalid data returns error changeset" do
-  #     %Order{id: order_id} = order = OrderFixtures.create()
+      assert {:ok, %Order{}} = Checkout.update_order(order, @update_attrs)
+    end
 
-  #     assert {:error, %Ecto.Changeset{}} =
-  #              Checkout.update_order(order, @invalid_attrs)
+    @tag :wip
+    test "update_order/2 with invalid data returns error changeset" do
+      %Order{id: order_id} = order = OrderFixtures.create()
 
-  #     assert order == Checkout.get_order!(order_id)
-  #   end
+      assert {:error, %Ecto.Changeset{}} =
+               Checkout.update_order(order, @invalid_attrs)
 
-  #   @tag :skip
-  #   test "delete_order/1 deletes the order" do
-  #     %Order{id: order_id} = order = OrderFixtures.create()
-  #     assert {:ok, %Order{}} = Checkout.delete_order(order)
-  #     assert_raise Ecto.NoResultsError, fn -> Checkout.get_order!(order_id)
-  # end
-  #   end
+      assert order == Checkout.get_order!(order_id)
+    end
 
-  #   @tag :skip
-  #   test "change_order/1 returns a order changeset" do
-  #     %Order{} = order = OrderFixtures.create()
-  #     assert %Ecto.Changeset{} = Checkout.change_order(order)
-  #   end
+    @tag :wip
+    test "delete_order/1 deletes the order" do
+      %Order{id: order_id} = order = OrderFixtures.create()
+      assert {:ok, %Order{}} = Checkout.delete_order(order)
+      assert_raise Ecto.NoResultsError, fn -> Checkout.get_order!(order_id) end
+    end
 
-  #   @tag :skip
-  #   test "preload_order/2 returns a preloadad order" do
-  #     %Order{} = order = OrderFixtures.create()
-
-  #     %Order{order_items: order_items} =
-  #       Checkout.preload_order(order, [:order_items])
-
-  #     assert order_items == []
-  #   end
-  # end
+    @tag :wip
+    test "change_order/1 returns an order changeset" do
+      %Order{} = order = OrderFixtures.create()
+      assert %Ecto.Changeset{} = Checkout.change_order(order)
+    end
+  end
 end
