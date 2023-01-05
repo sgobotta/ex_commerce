cb7f461e9754e10b2dd901652c11b0ebe36fb75f
Santiago Botta <sgobotta@gmail.com>
2023-01-05 20:35:38 -0300

    :computer: Update endpoint and root.html static routes

--- lib/ex_commerce_web/endpoint.ex
+++ lib/ex_commerce_web/endpoint.ex
@@ -25,7 +25,7 @@ defmodule ExCommerceWeb.Endpoint do
     at: "/",
     from: :ex_commerce,
     gzip: true,
-    only: ~w(css fonts images js favicon.ico robots.txt uploads)
+    only: ~w(assets fonts images favicon.ico robots.txt uploads)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
