c50b8d95c129f7a7a25193e68d0e11d781a8b4ca
Santiago Botta <sgobotta@gmail.com>
2023-01-05 21:42:51 -0300

    :computer: Update Cldr module

--- lib/ex_commerce_web/cldr.ex
+++ lib/ex_commerce_web/cldr.ex
@@ -4,14 +4,14 @@ defmodule ExCommerceWeb.Cldr do
   """
   use Cldr,
     default_locale: "en",
-    locales: ["en", "es"],
+    locales: ["en"],
     add_fallback_locales: false,
     gettext: ExCommerceWeb.Gettext,
     data_dir: "./priv/cldr",
     otp_app: :ex_commerce,
     precompile_number_formats: ["¤¤#,##0.##"],
     precompile_transliterations: [],
-    providers: [Cldr.Number],
+    providers: [],
     generate_docs: true,
     force_locale_download: Mix.env() == :prod
 end
