2d2c97fa19929fdf47f26b3675c0cb253685a7c5
Santiago Botta <sgobotta@gmail.com>
2023-01-07 03:04:54 -0300

    :computer: Update catalogue listing liveview

--- test/ex_commerce_web/live/catalogue_live_test.exs
+++ test/ex_commerce_web/live/catalogue_live_test.exs
@@ -99,7 +99,7 @@ defmodule ExCommerceWeb.CatalogueLiveTest do
         live(conn, Routes.catalogue_index_path(conn, :index, brand_id))
 
       assert index_live
-             |> element("#catalogue-#{catalogue_id} a", "Edit")
+             |> element("#catalogue-#{catalogue_id} a#update")
              |> render_click() =~
                "Edit Catalogue"
 
@@ -182,7 +182,7 @@ defmodule ExCommerceWeb.CatalogueLiveTest do
         live(conn, Routes.catalogue_index_path(conn, :index, brand_id))
 
       assert index_live
-             |> element("#catalogue-#{catalogue_id} a", "Delete")
+             |> element("#catalogue-#{catalogue_id} a#delete")
              |> render_click()
 
       refute has_element?(index_live, "#catalogue-#{catalogue_id}")
